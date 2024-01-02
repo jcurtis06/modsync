513c29f93657de9cdbf0281f227be28e52e19684
jcurtis06 <06jcurtis@gmail.com>
2024-01-01 19:05:00 -0500

    fetch packs

--- src/app/dashboard/components/YourPacks.tsx
+++ src/app/dashboard/components/YourPacks.tsx
@@ -1,13 +1,44 @@
-import Link from "next/link";
+"use client";
+
+import { useUser } from "@/hooks/useUser";
+import { supabase } from "@/supabaseClient";
+import { useEffect, useState } from "react";
 
 export default function YourPacks() {
-  return (
-    <div>
-      <p className="font-bold">Installed Modpacks</p>
+  const user = useUser();
+  const [modpacks, setModpacks] = useState([] as Modpack[]);
 
-      <p>
-        You don't have any modpacks yet, download or create one to get started
-      </p>
+  useEffect(() => {
+    if (!user) return;
+
+    // get the user's modpacks
+    fetchModpacks();
+  }, [user]);
+
+  const fetchModpacks = async () => {
+    if (!user) return;
+    const { data, error } = await supabase
+      .from("modpacks")
+      .select("*")
+      .eq("user_id", user.id);
+
+    if (error) {
+      console.error(error);
+    }
+
+    if (data) {
+      setModpacks(data);
+    }
+
+    console.log(data);
+  };
+
+  return (
+    <div className="p-4">
+      <p className="text-lg font-bold mb-2">Your Packs</p>
+      {modpacks.map((modpack, index) => (
+        <div key={index}>{modpack.name}</div>
+      ))}
     </div>
   );
 }
